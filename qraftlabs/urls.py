940daf4992ed900a38d3cbcb987320dd2fe55945
Emma Bryan Wandera <68502967+BryanWandera@users.noreply.github.com>
2023-07-28 12:54:50 +0300

    added assets

--- qraftlabs/urls.py
+++ qraftlabs/urls.py
@@ -2,9 +2,14 @@
 from django.contrib import admin
 from django.urls import path
 from labswebsite.views import *
+from qraftlabs import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home_view ),
     path('', home_view ),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
